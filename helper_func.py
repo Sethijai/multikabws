1ae998c1927573b6dfed4f8e3905c7ca657e049d
Sethijai <148748128+Sethijai@users.noreply.github.com>
2025-09-22 14:16:24 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -174,3 +174,27 @@ async def get_messages(client, message_ids, channel_id, bot_token: str = None):
     else:
         print(f"No messages fetched for IDs {message_ids} in channel {channel_id}")
     return messages
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    hmm = len(time_list)
+    for x in range(hmm):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += f"{time_list.pop()}, "
+    time_list.reverse()
+    up_time += ":".join(time_list)
+    return up_time
+
+subscribed = filters.create(is_subscribed)
+       
